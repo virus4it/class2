7d0f022605dfcd9034250185f0bdf19783cb6f12
virus4it <143338599+virus4it@users.noreply.github.com>
2024-10-08 19:42:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+class House:
+    def __init__(self,name,number_of_floors):
+        self.name=name
+        self.number_of_floor = number_of_floors
+    def go_to(self,new_floor):
+        floor=0
+        if new_floor > self.number_of_floor:
+            print("такого этажа нет")
+        else:
+            for i in range(new_floor):
+
+                floor = floor + 1
+                print(floor)
+    def __len__(self):
+        return self.number_of_floor
+    def __str__(self):
+        return f"Название:{self.name}, Кол-во этажей {self.number_of_floor} "
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
